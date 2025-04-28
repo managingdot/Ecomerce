37785780c6857c67d95f34270415d3f3b70e6176
managingdot <srk73330@gmail.com>
2025-04-28 14:05:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menLink = document.querySelector("#men a");
+
+//     menLink.addEventListener("click", function () {
+//         this.parentElement.classList.toggle("men-active");
+//         // Let the link navigate normally
+//     });
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menLink = document.querySelector("#men a");
+
+//     menLink.addEventListener("click", function () {
+//         this.parentElement.classList.toggle("women-active");
+//         // Let the link navigate normally
+//     });
+// });
+
+
+var a =10;
+var b = 3;
+console.log(a+b)
